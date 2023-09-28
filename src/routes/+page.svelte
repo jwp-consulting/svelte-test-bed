d18435b28920f32b200a974ce38b9af9ac5267f4
Justus Perlwitz <hello@justus.pw>
2023-09-28 22:13:22 +0900

    Add some code to try out instantiating Sarus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,19 @@
 <script lang="ts">
+  import {onMount} from "svelte";
+  import Sarus from "@anephenix/sarus";
+  function makeSarus(): Sarus {
+    return new Sarus({ url: "bogus-url"});
+  }
+
+  function action() {
+    console.log("Constructing sarus");
+    const sarus = makeSarus();
+    console.log(sarus);
+  }
+
+  onMount(() => {
+    action();
+  });
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
